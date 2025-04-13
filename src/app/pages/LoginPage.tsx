106fc7b2a5a5be8eb32732446b649f4dea700a16
m1rage <miragela.code@gmail.com>
2025-04-13 16:18:27 +0200

    refactor: restructure styles and components; remove unused files

---
+++ src/app/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "../styles/page.module.css";
+
+type Props = {}
+
+export default function LoginPage({}: Props) {
+  return (
+    <div className={styles.page}>
+      <main>
+        <p>Hello World!</p>
+      </main>
+    </div>
+  )
+}
