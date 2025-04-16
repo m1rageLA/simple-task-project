81dfecfdc990e08da38e5e754a59411deaf197ff
m1rage <miragela.code@gmail.com>
2025-04-16 09:31:42 +0200

    feat: enhance HomePage layout with user greeting and logout functionality; update styles

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,12 +13,30 @@ export default function HomePage({ }: Props) {
         setDisplayName(name);
     }, []);
 
+    const handleLogout = () => {
+        localStorage.removeItem("displayName");
+        window.location.href = "/";
+    };
+
     return (
         <div className={styles.page}>
-            <main>
-                <h1>Welcome <br />{displayName}</h1>
+            <main className={styles.main}>
+                {displayName ? (
+                    <>
+                        <h2>Welcome <br />{displayName}!</h2>
+                        <div className={styles.main__logout}>
+                            <a href="#" onClick={handleLogout} className={styles.button}>logout</a>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <h2>Welcome, Guest!</h2>
+                        <div className={styles.main__auth}>
+                            <a href="/" className={styles.button}>Login</a>
+                        </div>
+                    </>
+                )}
             </main>
         </div>
-
     );
 }
